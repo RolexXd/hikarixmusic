535bfd9277c4c2023ac0bc4e0271009a4de6dfa1
slayer <meerutuniversity996@gmail.com>
2026-04-01 08:43:17 +0530

    Create abuse.py

---
+++ AloneMusic/Slayer/Security/SPins/abuse.py
@@ -0,0 +1,10 @@
+from pyrogram import Client, filters
+from AloneMusic.Slayer.Security.Sdatabase import abuse_words
+
+@Client.on_message(filters.group & filters.text)
+async def abuse_filter(client, message):
+    async for w in abuse_words.find():
+        if w["word"] in message.text.lower():
+            await message.delete()
+            await message.reply("🚫 Abusive message removed")
+            return
