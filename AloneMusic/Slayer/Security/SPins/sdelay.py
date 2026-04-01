9c165bb175557cfa76f8573f448a47fff83810d2
slayer <meerutuniversity996@gmail.com>
2026-04-01 08:45:24 +0530

    Create sdelay.py

---
+++ AloneMusic/Slayer/Security/SPins/sdelay.py
@@ -0,0 +1,14 @@
+from pyrogram import Client, filters
+from AloneMusic.Slayer.Security.Sdatabase import groups
+
+@Client.on_message(filters.command("setdelay") & filters.group)
+async def setdelay(client, message):
+    if len(message.command) != 2 or not message.command[1].isdigit():
+        return
+    delay = int(message.command[1])
+    await groups.update_one(
+        {"chat_id": message.chat.id},
+        {"$set": {"delay": delay}},
+        upsert=True
+    )
+    await message.reply(f"🕒 Auto delete set to {delay}s")
