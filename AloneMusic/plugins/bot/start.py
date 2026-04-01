6927f15e4dad2d7189c7b3a4752c7a6e90d0b244
slayer <meerutuniversity996@gmail.com>
2026-04-01 09:05:26 +0530

    Update start.py

--- AloneMusic/plugins/bot/start.py
+++ AloneMusic/plugins/bot/start.py
@@ -5,11 +5,11 @@ from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from youtubesearchpython.__future__ import VideosSearch
 
-import config
-from Shigaraki import app
-from Shigaraki.misc import _boot_
-from Shigaraki.plugins.sudo.sudoers import sudoers_list
-from Shigaraki.utils.database import (
+import  config
+from AloneMusic import app
+from AloneMusic.misc import _boot_
+from AloneMusic.plugins.sudo.sudoers import sudoers_list
+from AloneMusic.utils.database import (
     add_served_chat,
     add_served_user,
     blacklisted_chats,
@@ -17,9 +17,9 @@ from Shigaraki.utils.database import (
     is_banned_user,
     is_on_off,
 )
-from Shigaraki.utils.decorators.language import LanguageStart
-from Shigaraki.utils.formatters import get_readable_time
-from Shigaraki.utils.inline import help_pannel, private_panel, start_panel
+from AloneMusic.utils.decorators.language import LanguageStart
+from AloneMusic.utils.formatters import get_readable_time
+from AloneMusic.utils.inline import help_pannel, private_panel, start_panel
 from config import BANNED_USERS, OWNER_ID
 from strings import get_string
 
